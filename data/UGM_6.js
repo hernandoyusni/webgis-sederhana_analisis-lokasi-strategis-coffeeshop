932073b0951e2ba4c54ccf3958d488ec94a22177
hernandoyusni <137862108+hernandoyusni@users.noreply.github.com>
2024-06-27 01:03:48 +0700

    Add files via upload

---
+++ data/UGM_6.js
@@ -0,0 +1,1 @@
+var json_UGM_6 = {"type":"FeatureCollection","name":"UGM_6","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"id":"0"},"geometry":{"type":"Point","coordinates":[110.377830232443699,-7.770088732084468]}}]}
